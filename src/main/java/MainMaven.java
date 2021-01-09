b8702937e64c8bf8436e525c447f30e85bee798b
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-09 15:41:28 -0600

    reversed string using stingutils

--- src/main/java/MainMaven.java
+++ src/main/java/MainMaven.java
@@ -3,4 +3,13 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 public class MainMaven {
+
+    public static void main(String [] args){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("What is your full name?\n");
+        String userString = userInput.nextLine();
+        System.out.println("Reverse: " + StringUtils.reverse(userString));
+    }
+
 }
